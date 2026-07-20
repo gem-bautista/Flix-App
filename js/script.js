b25a03283b29e883b546006e9d3ba4c8220b9d51
Gem Bautista <gemmatthew100@gmail.com>
2026-07-21 00:52:42 +0800

    add backdrop, remove hardcoded card in tv-details.html

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 const global = {
     currentPage: window.location.pathname
 }
@@ -6,7 +5,7 @@ const global = {
 async function displayPopularMovies() {
 
     const { results } = await fetchAPIData('movie/popular')
-
+    console.log(results)
     results.forEach(movie => {
         const div = document.createElement('div')
         div.classList.add('card')
@@ -33,7 +32,7 @@ async function displayPopularShows() {
 
     const { results } = await fetchAPIData('tv/popular')
 
-    console.log(results)
+
     results.forEach(show => {
         const div = document.createElement('div')
         div.classList.add('card')
@@ -54,17 +53,160 @@ async function displayPopularShows() {
 
         document.querySelector('#popular-shows').appendChild(div)
     })
+
+    console.log(results)
+}
+
+// fetch the specific movie when clicking the image.
+async function displayMovieDetails() {
+    const movieId = window.location.search.split('=')[1]
+    //the movie here is an object
+    const movie = await fetchAPIData(`movie/${movieId}`)
+    //Overlay for background image
+    displayBackgroundImage('movie', movie.backdrop_path)
+    const div = document.createElement('div')
+    div.innerHTML = `
+     <div class="details-top">
+          <div>
+          ${movie.poster_path ? `<img
+            src='https://image.tmdb.org/t/p/w500${movie.poster_path}'
+            class="card-img-top"
+            alt="Movie Title"
+        />` : `<img
+            src="images/no-image.jpg"
+            class="card-img-top"
+            alt="Movie Title"
+        />`
+        }
+          </div>
+          <div>
+            <h2>${movie.title}</h2>
+            <p>
+              <i class="fas fa-star text-primary"></i>
+              ${movie.vote_average.toFixed(1)} / 10
+            </p>
+            <p class="text-muted">Release Date: ${movie.release_date}</p>
+            <p>
+              ${movie.overview}
+            </p>
+            <h5>Genres</h5>
+            <ul class="list-group">
+                ${movie.genres.map(genre => {
+            return `<li>${genre.name}</li>`
+        }).join('')}
+            </ul>
+            <a href="${movie.homepage}" target="_blank" class="btn">Visit Movie Homepage</a>
+          </div>
+        </div>
+        <div class="details-bottom">
+          <h2>Movie Info</h2>
+          <ul>
+            <li><span class="text-secondary">Budget:</span> $${addCommasToNumber(movie.budget)}</li>
+            <li><span class="text-secondary">Revenue:</span> $${addCommasToNumber(movie.revenue)}</li>
+            <li><span class="text-secondary">Runtime:</span> ${movie.runtime} minutes</li>
+            <li><span class="text-secondary">Status:</span> ${movie.status}</li>
+          </ul>
+          <h4>Production Companies</h4>
+          <div class="list-group">
+          ${movie.production_companies.map((company) => `<span>${company.name}</span>`).join(', ')}
+          </div>
+        </div>
+    `
+    const parentDiv = document.getElementById('movie-details')
+    parentDiv.appendChild(div)
+}
+
+async function displayShowDetails() {
+    const showId = window.location.search.split('=')[1]
+    //the show here is an object
+    const show = await fetchAPIData(`tv/${showId}`)
+    //Overlay for background image
+    console.log(show)
+    displayBackgroundImage('show', show.backdrop_path)
+    const div = document.createElement('div')
+    div.innerHTML = `
+     <div class="details-top">
+          <div>
+          ${show.poster_path ? `<img
+            src='https://image.tmdb.org/t/p/w500${show.poster_path}'
+            class="card-img-top"
+            alt="Show Title"
+        />` : `<img
+            src="images/no-image.jpg"
+            class="card-img-top"
+            alt="Show Title"
+        />`
+        }
+          </div>
+          <div>
+            <h2>${show.name}</h2>
+            <p>
+              <i class="fas fa-star text-primary"></i>
+              ${show.vote_average.toFixed(1)} / 10
+            </p>
+            <p class="text-muted">Release Date: ${show.first_air_date}</p>
+            <p>
+              ${show.overview}
+            </p>
+            <h5>Genres</h5>
+            <ul class="list-group">
+                ${show.genres.map(genre => {
+            return `<li>${genre.name}</li>`
+        }).join('')}
+            </ul>
+            <a href="${show.homepage}" target="_blank" class="btn">Visit Show Homepage</a>
+          </div>
+        </div>
+        <div class="details-bottom">
+          <h2>Show Info</h2>
+          <ul>
+            <li><span class="text-secondary">Number of Episodes:</span> ${show.number_of_episodes}</li>
+            <li><span class="text-secondary">Last Episode to Air:</span>  ${show.last_episode_to_air.name}</li>
+            <li><span class="text-secondary">Status:</span> ${show.status}</li>
+          </ul>
+          <h4>Production Companies</h4>
+          <div class="list-group">
+          ${show.production_companies.map((company) => `<span>${company.name}</span>`).join(', ')}
+          </div>
+        </div>
+    `
+    const parentDiv = document.getElementById('show-details')
+    parentDiv.appendChild(div)
 }
 
 
+// Display backdrop on details pages
+function displayBackgroundImage(type, backgroundPath) {
+    const overlayDIV = document.createElement('div')
+    overlayDIV.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${backgroundPath})`
+    overlayDIV.style.backgroundSize = 'cover'
+    overlayDIV.style.backgroundPosition = 'center'
+    overlayDIV.style.backgroundRepeat = 'no-repeat'
+    overlayDIV.style.height = '100vh'
+    overlayDIV.style.width = '100vw'
+    overlayDIV.style.position = 'absolute'
+    overlayDIV.style.top = '0'
+    overlayDIV.style.left = '0'
+    overlayDIV.style.zIndex = '-1'
+    overlayDIV.style.opacity = '0.1'
+
+    if (type == 'movie') {
+        document.querySelector('#movie-details').appendChild(overlayDIV)
+    } else {
+        document.querySelector('#show-details').appendChild(overlayDIV)
+    }
+
+}
+
+// Used when loading
 function showSpinner() {
     document.querySelector('.spinner').classList.add('show')
 }
 
+// Hide after fetch
 function hideSpinner() {
     document.querySelector('.spinner').classList.remove('show')
 }
-
 
 //Fetch data from TMDB API
 async function fetchAPIData(endpoint) {
@@ -90,6 +232,11 @@ function highlightActiveLink() {
     })
 }
 
+function addCommasToNumber(number) {
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+}
+
+
 // Init App
 function init() {
     switch (global.currentPage) {
@@ -101,10 +248,10 @@ function init() {
             displayPopularShows()
             break
         case '/movie-details.html':
-            console.log('Movie details')
+            displayMovieDetails()
             break
         case '/tv-details.html':
-            console.log('Tv details')
+            displayShowDetails()
             break
         case '/search.html':
             console.log('Search')
