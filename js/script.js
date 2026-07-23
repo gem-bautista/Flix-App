fa49ee2274b242b2205a02f970b61c09db404e38
Gem Bautista <gemmatthew100@gmail.com>
2026-07-23 20:17:05 +0800

    finish functionality

--- js/script.js
+++ js/script.js
@@ -4,7 +4,8 @@ const global = {
         term: '',
         type: '',
         page: 1,
-        totalPages: 1
+        totalPages: 1,
+        totalResults: 0
     },
     api: {
         apiKey: '528dc943d2c749317cd4c05633e7940f',
@@ -32,7 +33,8 @@ async function searchAPIData() {
 
     showSpinner()
 
-    const response = await fetch(`${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}`)
+    const response = await fetch(`${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}&page=${global.search.page}`)
+
     const data = await response.json()
 
     hideSpinner()
@@ -46,9 +48,12 @@ async function search() {
     global.search.type = urlParams.get('type')
     global.search.term = urlParams.get('search-term')
 
-    console.log(global.search.type)
     if (global.search.term !== '' && global.search.term !== null) {
-        const { results, total_pages, page } = await searchAPIData()
+        const { results, total_pages, page, total_results } = await searchAPIData()
+
+        global.search.page = page
+        global.search.totalPages = total_pages
+        global.search.totalResults = total_results
 
         if (results.length === 0) {
             showAlert('No results found.')
@@ -57,13 +62,19 @@ async function search() {
 
         displaySearchResults(results)
 
-        console.log(results)
+
     } else {
         showAlert('Please enter a search term')
     }
 }
 
 function displaySearchResults(results) {
+
+    //Clear previous results
+    document.querySelector('#search-results').innerHTML = ''
+    document.querySelector('#search-results-heading').innerHTML = ''
+    document.querySelector('#pagination').innerHTML = ''
+
     results.forEach(result => {
         const div = document.createElement('div')
         div.classList.add('card')
@@ -82,8 +93,50 @@ function displaySearchResults(results) {
           </p>
         </div>`
 
+        document.querySelector('#search-results-heading').innerHTML = `
+        <h2>${results.length} of ${global.search.totalResults} for ${global.search.term}</h2>
+        `
         document.querySelector('#search-results').appendChild(div)
     })
+
+    displayPagination()
+}
+
+//Create and display pagination for search
+function displayPagination() {
+    const div = document.createElement('div')
+    div.classList.add('pagination')
+    div.innerHTML = `   
+        <button class="btn btn-primary" id="prev">Prev</button>
+        <button class="btn btn-primary" id="next">Next</button>
+        <div class="page-counter">Page ${global.search.page} of ${global.search.totalPages}</div>`
+
+    document.querySelector('#pagination').appendChild(div)
+
+    //disable prev button if on first page
+    if (global.search.page === 1) {
+        document.querySelector('#prev').disabled = true
+    }
+    //disable next button if on last page
+    if (global.search.page === global.search.totalPages) {
+        document.querySelector('#next ').disabled = true
+    }
+
+    //Next Page
+    document.querySelector('#next').addEventListener('click', async () => {
+        global.search.page++
+        const { results, total_pages } = await searchAPIData()
+        displaySearchResults(results)
+    })
+
+    //previous Page
+    document.querySelector('#prev').addEventListener('click', async () => {
+        global.search.page--
+        const { results, total_pages } = await searchAPIData()
+        displaySearchResults(results)
+    })
+
+
 
 }
 
@@ -137,8 +190,6 @@ async function displayPopularShows() {
 
         document.querySelector('#popular-shows').appendChild(div)
     })
-
-    console.log(results)
 }
 
 // fetch the specific movie when clicking the image.
